1b39b72d1bea99d5890423e20c9722d6f1c35571
Kuldeep <c0dysharma01@gmail.com>
2022-08-28 22:58:36 +0530

    created Tailwind UI

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        darkCyan: 'hsl(158, 36%, 37%)',
+        cream: 'hsl(30, 38%, 92%)',
+        darkBlue: 'hsl(212, 21%, 14%)',
+        darkGrayishBlue: 'hsl(228, 12%, 48%)',
+        white: 'hsl(0, 0%, 100%)',
+        moreDarkCyan: '#184131'
+      },
+      fontFamily:{
+        fraunces: ['Fraunces'],
+        montserrat: ['Montserrat']
+      },
+      backgroundImage: {
+        'imageMobile': "url('../src/images/image-product-mobile.jpg')",
+        'imageDesktop': "url('../src/images/image-product-desktop.jpg')",
+      }
+    },
+  },
+  plugins: [],
+}
